df5138c8b8bea602ed413430b9edc37c6561c7f4
ken <ken@iamcoding.com>
2013-12-30 00:46:02 +0800

    Create

---
+++ FileMd5Gen/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace FileMd5Gen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.choosePath = new System.Windows.Forms.TextBox();
+            this.info = new System.Windows.Forms.ListBox();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // choosePath
+            // 
+            this.choosePath.Location = new System.Drawing.Point(89, 40);
+            this.choosePath.Name = "choosePath";
+            this.choosePath.Size = new System.Drawing.Size(198, 21);
+            this.choosePath.TabIndex = 0;
+            // 
+            // info
+            // 
+            this.info.FormattingEnabled = true;
+            this.info.ItemHeight = 12;
+            this.info.Location = new System.Drawing.Point(89, 75);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(554, 280);
+            this.info.TabIndex = 1;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(349, 37);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(75, 23);
+            this.btnChoose.TabIndex = 2;
+            this.btnChoose.Text = "选择目录";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(324, 373);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 23);
+            this.btnGen.TabIndex = 3;
+            this.btnGen.Text = "生成";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 408);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.choosePath);
+            this.Name = "Form1";
+            this.Text = " 文件Md5生成器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox choosePath;
+        private System.Windows.Forms.ListBox info;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Button btnGen;
+    }
+}
+
---
+++ FileMd5Gen/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileMd5Gen
+{
+    public partial class Form1 : Form
+    {
+        private FolderBrowserDialog folderBrowser;
+        private StringBuilder fileInfoes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            folderBrowser = new FolderBrowserDialog();
+            fileInfoes = new StringBuilder();
+        }
+
+        //Generate
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+
+            if (choosePath.Text.Length == 0)
+            {
+                MessageBox.Show("Please choose the direction.");
+                return;
+            }
+            //Clear data
+            fileInfoes.Clear();
+            info.Items.Clear();
+
+            //Generate md5 for each file
+            String[] fileNames = Directory.GetFiles(choosePath.Text, "*", SearchOption.AllDirectories);
+            foreach (String fileName in fileNames)
+            {
+                string fileNameShort = fileName.Substring(choosePath.Text.Length+1);
+                string md5 = GetMD5HashFromFile(fileName);
+
+                fileInfoes.Append(fileNameShort).Append(",").Append(md5).Append("\n");
+                info.Items.Add(fileNameShort+","+md5);
+            }
+            //save file
+            saveFile();
+        }
+
+        //Save file
+        private void saveFile()
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "version.txt";
+            dlg.Filter = "txt文件|*.txt";
+            dlg.AddExtension = true;
+            dlg.Title = "保存文件";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(dlg.FileName, FileMode.Create);
+                byte[] data = new UTF8Encoding().GetBytes(fileInfoes.ToString());
+                fs.Write(data, 0, data.Length);
+                fs.Flush();
+                fs.Close();
+            }
+        }
+
+        //Choose Directory
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                choosePath.Text = folderBrowser.SelectedPath;
+            }
+        }
+
+        //Generate file's md5 value
+        private static string GetMD5HashFromFile(string fileName)
+        {
+            using(FileStream file = new FileStream(fileName, FileMode.Open)){
+                System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] hash = md5.ComputeHash(file);
+   
+                StringBuilder result = new StringBuilder();
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    result.Append(hash[i].ToString("x2"));
+                }
+                return result.ToString();
+            }
+        }
+    }
+}
